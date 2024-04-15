d510875e11c074612ac33fd83a4186021e67bfda
jujufontch <144907131+jujufontch@users.noreply.github.com>
2024-04-15 13:28:26 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 
 public class Main {
-    public static void main(String[] args) {
+    
+     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
@@ -22,101 +23,107 @@ public class Main {
         String passwort ="";
 
         int a = 0;
-        do{
+
+        try {
+            do {
 
 
-
-        System.out.println("Welcome to Cameroon's International Bank\n");
-        System.out.println("Haben sie schon einen Bankkonto ? \n");
-        System.out.println("  Geben sie bitte ein \n1. Falls ja \n 2 Falls nein ");
-        System.out.println("0. Fall sie die Amwendung beenden wollen ");
-
+                System.out.println("Welcome to Cameroon's International Bank\n");
+                System.out.println("Haben sie schon einen Bankkonto ? \n");
+                System.out.println("  Geben sie bitte ein \n1 : Falls ja \n 2 : Falls nein ");
+                System.out.println("0 : Fall sie die Amwendung beenden wollen ");
 
 
-        scann= scanner.nextInt();
-
-        switch(scann){
-
-            case 1:
-                UserIdentifier id = new UserIdentifier();
-                id.Benutzer();
-                break;
-
-            case 2:
-                System.out.println("Wollen sie einen Konto erstellen?\n");
-                System.out.println("  Geben sie bitte ein \n1. Falls ja \n 2 Falls nein \n0. Falls sie zurück wollen");
-                scann1= scanner.nextInt();
+                scann = (int) scanner.nextInt();
 
 
-                do {
-                    switch(scann1){
-                        case 1:
+                switch (scann) {
 
-                            UserCreator creator = new UserCreator();
+                    case 1:
+                        UserIdentifier id = new UserIdentifier();
+                        id.Benutzer();
+                        break;
 
-                            Benutzer  object =creator.create();
+                    case 2:
+                        System.out.println("Wollen sie einen Konto erstellen?\n");
+                        System.out.println("  Geben sie bitte ein \n 1 : Falls ja \n 2 : Falls nein \n0 : Falls sie zurück wollen");
+                        scann1 = scanner.nextInt();
 
-                            System.out.println("Was sollen sie jetzt machen ?\n\n");
-                            System.out.println("Geben sie 1 ein .Falls sie ihrkonto gleich steuern möchten\n");
-                            System.out.println("2 .Falls sie zum Hauptmenü gehen wollen ");
-                            scann2= scanner.nextInt();
 
-                            do{
+                        switch (scann1) {
+                            case 1:
 
-                                switch(scann2){
+                                UserCreator creator = new UserCreator();
+
+                                Benutzer object = creator.create();
+
+                                System.out.println("Was wollen sie jetzt machen ?\n\n");
+                                System.out.println("Geben sie ein \n 1 : Falls sie ihr konto gleich steuern möchten\n");
+                                System.out.println("2 : Falls sie zum Hauptmenü gehen wollen ");
+                                scann2 = scanner.nextInt();
+
+
+                                switch (scann2) {
 
                                     case 1:
-                                        KontoSteuern ks= new KontoSteuern();
+                                        KontoSteuern ks = new KontoSteuern();
 
                                         ks.manage(object);
                                         break;
 
                                     case 2:
                                         System.out.println("Sie werden zum Hauptmenü weitergeleitet ");
-                                        scann1=0;
+                                        scann1 = 0;
                                         break;
 
                                     default:
-                                        System.out.println("Falsche Einagbe , versuchen sie es bitte erneut");
+                                        System.out.println("Falsche Eingabe , versuchen sie es bitte erneut");
                                         break;
 
                                 }
-                            }while(scann2!=2);
-                            break;
 
-                        case 2:
-                            System.out.println("Okay ");
-                            break;
+                                break;
 
-
-                        case 0 :
-                            System.out.println("Okay dann sie werden zum Hauptmenü weitergeleitet");
-                            scann1=0;
-                            break;
-
-                        default:
-                            System.out.println("Falsche Eingabe , versucht es erneut");
-                            break;
+                            case 2:
+                                System.out.println("Okay ");
+                                break;
 
 
-                    }
-                }while(scann!= 0);
-                break;
-            case 0:
-                System.out.println("Die Anwendung wird beendet");
-                break;
+                            case 0:
+                                System.out.println("Okay dann sie werden zum Hauptmenü weitergeleitet");
+                                scann1 = 0;
+                                break;
 
-            default:
-                System.out.println("Falsche Eingabe , versuchen sie es bitte erneut");
-                break;
-             }
+                            default:
+                                System.out.println("Falsche Eingabe , versucht es erneut");
+                                break;
 
 
+                        }
 
 
-        } while(scann!=0);
+                    case 0:
+                        System.out.println("Die Anwendung wird beendet");
+                        break;
+
+                    default:
+                        System.out.println("Falsche Eingabe , versuchen sie es bitte erneut");
+                        break;
+                }
+
+
+            } while (scann != 0);
+
+        }
+        catch (Exception e ){
+
+            System.out.println("Falsche Eingabe\n");
+            System.out.println("Die Anwendung wird jetzt beendet \n");
+
+        }
 
     }
+
 
     public static Boolean nameprüfer(String name){
 
